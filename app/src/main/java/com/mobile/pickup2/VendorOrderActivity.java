9a2aa288db8b98e822f4f81e3ee3fbd1b89152ae
Steven <bonillasc@gmail.com>
2017-03-28 15:31:44 -0400

    function skeleton to populate list_view for customer's orders

--- app/src/main/java/com/mobile/pickup2/VendorOrderActivity.java
+++ app/src/main/java/com/mobile/pickup2/VendorOrderActivity.java
@@ -2,6 +2,8 @@ package com.mobile.pickup2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,8 +26,14 @@ public class VendorOrderActivity extends AppCompatActivity
 
     }
 
+    //creating the ListView of CustomerOrder objects using array Adapter
     private void createOrderObject()
-    {}
+    {
+        ArrayAdapter<CustomerOrder> adapter1 = new MyListAdapter();
+        ListView list1 = (ListView) findViewById(R.id.order_list_view);
+        list1.setAdapter(adapter1);
+        
+    }
 
 
     //arbitrary values to place into Order List
@@ -34,7 +42,7 @@ public class VendorOrderActivity extends AppCompatActivity
         HashMap<String, Integer> sotired = new HashMap<String,Integer>();
         sotired.put("ham", 3);
         sotired.put("dirt", 4);
-        sotired.put("shit", 1);
+        sotired.put("lol", 1);
         CustomerOrder order1 = new CustomerOrder("bob1",R.drawable.mouse,false,sotired);
         orders.add(order1);
 
